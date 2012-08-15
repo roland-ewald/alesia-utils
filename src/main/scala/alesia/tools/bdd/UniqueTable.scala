d7dc5db5aa89750b82df2059e0a8558859f12c25
Roland Ewald <roland.ewald@uni-rostock.de>
2012-08-15 16:35:42 +0200

    bdd: added stub for unique table.

---
+++ src/main/scala/alesia/tools/bdd/UniqueTable.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2012 Roland Ewald
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package alesia.tools.bdd
+
+/** A very simple unique table implementation, roughly following the scheme described by D. E. Knuth in 'The Art of Computer Programming', vol. 4,
+ *  fascicle 1, p. 92 et sqq.
+ *  @author Roland Ewald
+ */
+class UniqueTable {
+
+  private[this] var currentMaxVar = 0
+  
+  
+}
